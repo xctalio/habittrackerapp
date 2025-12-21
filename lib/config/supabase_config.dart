3e9510354972bdd29335659d0c8d6e9f1e50a09b
Octa R. <135010215+xctalio@users.noreply.github.com>
2025-12-21 19:16:55 +0700

    Update SupabaseConfig to remove credentials
    Removed sensitive Supabase credentials from the configuration.

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,4 +1,4 @@
 class SupabaseConfig {
-  static const String url = 'https://mhgqnkujdyywununbvts.supabase.co/';
-  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oZ3Fua3VqZHl5d3VudW5idnRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3OTA3MDYsImV4cCI6MjA3OTM2NjcwNn0.hhZsnYOsyaqc28wODAiuLHIgq3TANK5DlrI-qfHAnPA';
+  static const String url = 'https://.supabase.co/';
+  static const String anonKey = '';
 }
